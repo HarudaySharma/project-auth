9311ca766ab73351b960c64751e5796dbd881786
HarudaySharma <harudayvasist@gmail.com>
2023-12-16 06:35:17 +0530

    tested the route on server side

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,9 @@
+export const user = (req, res) => {
+    res.json(
+        {
+            message: "API working",
+        }
+    )
+}
+
+// using this user fuction for the user route
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+
 dotenv.config();
 
 mongoose
@@ -18,3 +20,7 @@ app.listen(3000, () => {
   console.log("Server is listening on port:3000");
 
 });
+
+//to create an api route
+
+app.use("/backend/user/", userRoutes);
