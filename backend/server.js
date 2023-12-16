32faa5bfe8afb01d0510424562c7ff40d63136f5
HarudaySharma <harudayvasist@gmail.com>
2023-12-16 05:59:30 +0530

    connected to the mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to the db");
+  })
+  .catch((msg) => {
+    console.log(msg);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log("Server is listening on port:3000");
-})
+  console.log("Server is listening on port:3000");
+
+});
